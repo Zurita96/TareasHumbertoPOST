e01ec510b908c40cf19a8fcef66f51da911ddb42
Humberto <betozurita_@hotmail.com>
2023-05-30 18:40:50 -0500

    primer commit

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+const posts = document.querySelector ("#posts")
+const post = document.querySelectorAll (".post")
+const countpost = document.querySelector ("#countpost") /*Los ID se llaman con #*/
+const showpost = document.querySelector ("#showpost")
+
+posts.style.display = "none"
+countpost.innerHTML = post.length
+showpost.addEventListener ("click", function (){
+    posts.style.display = "grid"
+})
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then(response => response.json())
+.then(json => {
+    let template = ``
+    json.forEach(element => {
+        template+=`
+        <div class="post">
+            <h2>${element.userId}</h2>
+            <h3>${element.id}</h3>
+            <p>${element.title}</p>
+            <p>${element.body}</p>
+        </div>`
+        
+    });
+    posts.innerHTML = template
+})
